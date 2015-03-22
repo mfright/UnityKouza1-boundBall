127a2f28736620f29fd68532cb0b6b295274b2cb
mfright <uj020288@yahoo.co.jp>
2015-03-22 16:47:47 +0900

    sample of BoundBall
    Based on...
    
    https://www.youtube.com/watch?v=TLnSz6GtjHg

---
+++ blockBreak/Assets/BallStart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallStart : MonoBehaviour {
+
+	public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().AddForce (
+			(transform.forward + transform.right) * speed,
+			ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ blockBreak/Assets/CT.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CT : MonoBehaviour {
+
+	public float Accele = 1.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody>().AddForce(
+			transform.right*Input.GetAxisRaw("Horizontal")*Accele,ForceMode.Impulse);
+	}
+}
